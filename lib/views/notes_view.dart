6432be186303da47efa0b96ade69c1caf6b4d408
rafah <rafah5743@gmail.com>
2025-05-12 10:54:38 +0300

    finish appBar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -7,10 +8,8 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.dark
-      ),
+    return Scaffold(
+      body: NotesViewBody(),
     );
   }
 }
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+      children: [
+        SizedBox(height: 30),
+        CustomAppBar()
+      ],
+    )
+    );
+  }
+}
